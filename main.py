87e27930a8b92feabc41b804bd50ff0912875453
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-26 14:03:15 -0700

    first

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ def main():
     Code Your Program here
     ########################################
     """
-    duplication = 0
 
+    duplication = 0
+    if num1 == num2:
+        duplication = 1
+    elif num1 == num2 and num1 == num2:
+        duplication = 2
+    elif num2 == num3 and num2 == num1:
+        duplication = 2
+    elif num3 == num2 and num3 == num1:
+        duplication = 2
+    elif num1 == num2 and num1 == num2 and num1 == num3:
+        duplication = 3
+        
+    print(f'Duplication {duplication}')
     ########################################
     # Do not delete the return statement
     ########################################
