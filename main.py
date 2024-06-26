4e5a5de9c718256e6fa31359726e155a833857b9
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-26 14:06:43 -0700

    third

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     duplication = 0
     if num1 == num2 == num3:
         duplication = 3
-    elif num1 == num2 and num1 == num3 or num2 == num3 and num2 == num1 or num3 == num1 and num3 == num2:
+    elif num1 == num2 or num2 == num3 or num1 == num2:
         duplication = 2
     else:
         duplicaiton = 0
